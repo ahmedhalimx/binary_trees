39264f16904b62dbc6a5ac9d915c29b6b20c7a91
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-08-30 21:25:14 +0300

    added 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -18,10 +18,15 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		return (first->parent);
 
 	if (first == second)
-		return ((binary_tree_t *)first);
+		return (first->parent);
 
 	if (second->parent == first || second->parent == first->parent)
 		return (second->parent);
 
-	return (binary_trees_ancestor(first, second->parent));
+	if (first->parent && second->parent)
+		return (binary_trees_ancestor(first->parent, second->parent));
+	else if (!first->parent)
+		return (binary_trees_ancestor(first, second->parent));
+	else
+		return (binary_trees_ancestor(first->parent, second));
 }
