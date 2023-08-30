3df8d74e7aa8254a5281511ae93267c9e568957d
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-08-30 16:45:45 +0300

    1-binary

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,25 +14,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = binary_tree_node(parent, value);
 
 	if (!new_node)
 		return (NULL);
 
-	if (!parent->left)
+	if (parent->left)
 	{
-		parent->left = new_node;
-		new_node->parent = parent;
-		new_node->left = new_node->right = NULL;
-		new_node->n = value;
-		return (new_node);
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
 	}
 
-	new_node->left = parent->left;
-	parent->left->parent = new_node;
 	parent->left = new_node;
-	new_node->parent = parent;
-	new_node->n = value;
-
 	return (new_node);
 }
