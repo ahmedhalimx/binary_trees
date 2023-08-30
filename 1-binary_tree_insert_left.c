957d93069f0a43579ef75be0e8a3f66c424b2481
ahmedhalimx <ahmedhalimcs101@gmail.com>
2023-08-30 16:59:05 +0300

    s

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - Inserts a new_node,
- * as the left child of another node
+ * binary_tree_insert_left - Inserts a new_node as the left
+ * child of another node
  *
  * @parent: Parent node to which the new_node will be inserted left to
  * @value: new_node's value
